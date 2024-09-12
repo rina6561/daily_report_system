50590b1aca8ed094d85ea83dda67db5315469bfe
rina.goto <rina@jprs.co.jp>
2024-09-12 18:37:19 +0900

    ログアウトアクションの作成

--- src/main/java/actions/AuthAction.java
+++ src/main/java/actions/AuthAction.java
@@ -10,12 +10,11 @@ import constants.ForwardConst;
 import constants.MessageConst;
 import constants.PropertyConst;
 import services.EmployeeService;
-
 /**
  * 認証に関する処理を行うActionクラス
  *
  */
-public class AuthAction extends ActionBase{
+public class AuthAction extends ActionBase {
 
     private EmployeeService service;
 
@@ -23,7 +22,7 @@ public class AuthAction extends ActionBase{
      * メソッドを実行する
      */
     @Override
-    public void process() throws ServletException, IOException{
+    public void process() throws ServletException, IOException {
 
         service = new EmployeeService();
 
@@ -38,19 +37,19 @@ public class AuthAction extends ActionBase{
      * @throws ServletException
      * @throws IOException
      */
-    public void showLogin() throws ServletException, IOException{
+    public void showLogin() throws ServletException, IOException {
 
-      //CSRF対策用トークンを設定
+        //CSRF対策用トークンを設定
         putRequestScope(AttributeConst.TOKEN, getTokenId());
 
-      //セッションにフラッシュメッセージが登録されている場合はリクエストスコープに設定する
+        //セッションにフラッシュメッセージが登録されている場合はリクエストスコープに設定する
         String flush = getSessionScope(AttributeConst.FLUSH);
-        if(flush != null) {
+        if (flush != null) {
             putRequestScope(AttributeConst.FLUSH,flush);
             removeSessionScope(AttributeConst.FLUSH);
         }
 
-      //ログイン画面を表示
+        //ログイン画面を表示
         forward(ForwardConst.FW_LOGIN);
     }
 
@@ -59,7 +58,6 @@ public class AuthAction extends ActionBase{
      * @throws ServletException
      * @throws IOException
      */
-
     public void login() throws ServletException, IOException {
 
         String code = getRequestParam(AttributeConst.EMP_CODE);
@@ -99,4 +97,21 @@ public class AuthAction extends ActionBase{
         }
     }
 
+    /**
+     * ログアウト処理を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void logout() throws ServletException, IOException {
+
+        //セッションからログイン従業員のパラメータを削除
+        removeSessionScope(AttributeConst.LOGIN_EMP);
+
+        //セッションにログアウト時のフラッシュメッセージを追加
+        putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGOUT.getMessage());
+
+        //ログイン画面にリダイレクト
+        redirect(ForwardConst.ACT_AUTH, ForwardConst.CMD_SHOW_LOGIN);
+
+    }
 }
--- src/main/java/actions/TopAction.java
+++ src/main/java/actions/TopAction.java
@@ -11,34 +11,33 @@ import constants.ForwardConst;
  * トップページに関する処理を行うActionクラス
  *
  */
-public class TopAction extends ActionBase{
+public class TopAction extends ActionBase {
 
     /**
      * indexメソッドを実行する
      */
     @Override
-    public void process() throws ServletException, IOException{
+    public void process() throws ServletException, IOException {
 
         //メソッドを実行
-
         invoke();
+
     }
 
     /**
      * 一覧画面を表示する
      */
-    public void index() throws ServletException, IOException{
+    public void index() throws ServletException, IOException {
 
         //セッションにフラッシュメッセージが設定されている場合はリクエストスコープに移し替え、セッションからは削除する
         String flush = getSessionScope(AttributeConst.FLUSH);
-        if(flush != null) {
-            putRequestScope(AttributeConst.FLUSH,flush);
+        if (flush != null) {
+            putRequestScope(AttributeConst.FLUSH, flush);
             removeSessionScope(AttributeConst.FLUSH);
         }
 
         //一覧画面を表示
         forward(ForwardConst.FW_TOP_INDEX);
-
     }
 
 }
