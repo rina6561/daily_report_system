8317f301e6031cb694087545c84af33f066af8ec
rina.goto <rina@jprs.co.jp>
2024-09-16 21:05:01 +0900

    トップページアクションの作成

--- src/main/java/actions/TopAction.java
+++ src/main/java/actions/TopAction.java
@@ -1,16 +1,16 @@
 package actions;
 
 import java.io.IOException;
-import java.util.List; //追記
+import java.util.List; //追記、日報データを複数取得するため
 
 import javax.servlet.ServletException;
 
-import actions.views.EmployeeView; //追記
-import actions.views.ReportView; //追記
+import actions.views.EmployeeView; //追記、ログイン中の従業員のデータを取得・操作するために必要なDTOクラス
+import actions.views.ReportView; //追記、日報データを表すDTOクラス
 import constants.AttributeConst;
 import constants.ForwardConst;
-import constants.JpaConst;  //追記
-import services.ReportService;  //追記
+import constants.JpaConst;  //追記、1ページあたりの表示件数などの定数を使用するためのクラス
+import services.ReportService;  //追記、DBとのやり取りや処理
 
 /**
  * トップページに関する処理を行うActionクラス
@@ -18,7 +18,7 @@ import services.ReportService;  //追記
  */
 public class TopAction extends ActionBase {
 
-    private ReportService service; //追記
+    private ReportService service; //追記、データベースから日報データを取得
 
     /**
      * indexメソッドを実行する
@@ -26,12 +26,12 @@ public class TopAction extends ActionBase {
     @Override
     public void process() throws ServletException, IOException {
 
-        service = new ReportService(); //追記
+        service = new ReportService(); //追記、ReportService のインスタンスを生成→日報の取得や件数カウントの処理
 
         //メソッドを実行
         invoke();
 
-        service.close(); //追記
+        service.close(); //追記、サービスの終了処理
 
     }
 
